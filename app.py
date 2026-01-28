d36517dfa6b241d6b700d4cdc03f95a8dd30d3ed
Arodriguez <adrian.64sv@gmail.com>
2026-01-28 11:30:04 -0600

    Refactor sync logic to utilize main connection setup and streamline database operations

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine, text
 from tenacity import retry, wait_fixed, stop_after_attempt
 from sqlalchemy.exc import SQLAlchemyError
 import urllib
+from conn import main
 
 load_dotenv()
 
@@ -20,85 +21,25 @@ logging.basicConfig(
 )
 
 class Sync:
-    @retry(wait=wait_fixed(5), stop=stop_after_attempt(3))
-    def create_engine_connection(self, connection_string):
-        try:
-            engine = create_engine(connection_string)
-            connection = engine.connect()
-            print("Engine connection successful")
-            return connection
-        except Exception as e:
-            print("Error creating engine connection: ", e)
-            return None
-
-    def get_database_remote(self):
-        server = os.getenv('REMOTE_HOST')
-        database = os.getenv('REMOTE_DB')
-        username =  os.getenv('REMOTE_USER')
-        password = os.getenv('REMOTE_PASSWORD')
-
-        params = urllib.parse.quote_plus(
-            f"DRIVER={{ODBC Driver 18 for SQL Server}};"
-            f"SERVER={server};"
-            f"DATABASE={database};"
-            f"UID={username};"
-            f"PWD={password};"
-            "Encrypt=no;"
-            "TrustServerCertificate=yes;"
-            "Connection Timeout=30;"
-        )
-
-        try:
-            return self.create_engine_connection(f"mssql+pyodbc:///?odbc_connect={params}")
-
-        except SQLAlchemyError as e:
-            print("Error connecting to database: ", e)
-            return None
-
-
-    def get_database_local(self):
-        server =  os.getenv('LOCAL_HOST')
-        database = os.getenv('LOCAL_DB')
-        username = os.getenv('LOCAL_USER')
-        password = os.getenv('LOCAL_PASSWORD')
-
-        params = urllib.parse.quote_plus(
-            f"DRIVER={{ODBC Driver 18 for SQL Server}};"
-            f"SERVER={server};"
-            f"DATABASE={database};"
-            f"UID={username};"
-            f"PWD={password};"
-            "Encrypt=no;"
-            "TrustServerCertificate=yes;"
-            "Connection Timeout=30;"
-        )
-
-        try:
-            return self.create_engine_connection(f"mssql+pyodbc:///?odbc_connect={params}")
-
-        except SQLAlchemyError as e:
-            print("Error connecting to database: ", e)
-            return None
+    def __init__(self):
+        self.remote, self.local = main()
 
     def sync(self):
         try:
-            local = self.get_database_local()
-            remote = self.get_database_remote()
-
-            if not local or not remote:
+            if not self.local or not self.remote:
                 raise Exception("Database connections could not be established")
 
             result = None
-            with remote.begin():
-                local_result = local.execute(text("EXEC HOVISYS.dbo.SALES"))
+            with self.local.begin() as l:
+                local_result = l.execute(text("EXEC HOVISYS.dbo.SALES"))
                 result = local_result.fetchall()
             
             if not result:
                 raise Exception("No data to transfer")
             
             for row in result:
-                with remote.begin():
-                    remote_result = remote.execute(
+                with self.remote.begin() as r:
+                    remote_result = r.execute(
                         text("""
                             INSERT INTO [Ventas_RD].[dbo].[Ventas_SubwaySambilHoras]
                             (
@@ -129,25 +70,19 @@ class Sync:
                         }
                     )
                     if remote_result.rowcount != 0:
-                        remote.execute(text(
+                        r.execute(text(
                             "INSERT INTO [Ventas_RD].[dbo].[sync_control] (estado, mensaje)"
                             "VALUES ('SUCCESS', 'Data synchronized successfully')"
                         ))
                     else :
-                        remote.execute(text(
+                        r.execute(text(
                             "INSERT INTO [Ventas_RD].[dbo].[sync_control] (estado, mensaje)"
                             "VALUES ('FAILURE', 'Data synchronization failed')"
                         ))
-                    remote.commit()
+                    r.commit()
             print("Data transfer complete")
         except pyodbc.Error as e:
             print("Error during data transfer: ", e)
-        
-        finally:
-            if local:
-                local.close()
-            if remote:
-                remote.close()
 
     def store_open(self):
         from datetime import time
@@ -157,14 +92,14 @@ class Sync:
         return openAt <= now <= closeAt
 
 
-def main():
+def exec():
     """Función principal"""
     sincronizador = Sync()
     try:
         sincronizador.sync()
     except Exception as e:
-        logging.error(f"Error crítico: {e}")
+        logging.error(f"Error: {e}")
         exit(1)
 
 if __name__ == "__main__":
-    main()
+    exec()
